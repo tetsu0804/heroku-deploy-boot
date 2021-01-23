3b3fd964655dd1807291b3a78be39ca4a6b91959
tetsu <banginhoodyonezawa@gmail.com>
2021-01-23 13:40:41 +0900

    Hello Vue表示完了

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,9 @@ module HerokuDeployVueBootstrapVue
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.helper = false
+      g.asstes = false
+    end
   end
 end
